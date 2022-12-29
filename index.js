a98c2359c591207e72c77781e26d9ab199b0d9ba
Manmohandeep Singh <ltsbcsgo@gmail.com>
2022-12-29 01:06:13 -0500

    commit tree 19737c4ad05c57b903d99d08c5bc0b4c51b63dc0

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(name, reason) {
+    const names = []
+    let i = 0;
+    while (i < name.length) {
+        names.push(`Thank you, ${name[i]}, for the wonderful ${reason} gift!`);
+        i++
+    }
+    return names;
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n--)
+    }
+}
+
+
+
+
+
+
